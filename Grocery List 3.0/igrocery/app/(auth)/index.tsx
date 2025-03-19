0f3acccdda5ae84c71e1eff3bb326ed6378f4ea9
Arthur Quinellato <arthur.quinellato@gmail.com>
2025-03-19 19:05:23 -0300

    Restarting project

--- Grocery List 3.0/igrocery/app/(auth)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native'
-
-export default function WelcomeScreen() {
-    return (
-        <View>
-            <Text>Welcome to iGrocery</Text>
-        </View>
-    )
-}
